8cd1d2c11d4c7a8cd63c0f42f4ae0d86b8633d0d
AdrianPerez2207 <aperezsanchez441@gmail.com>
2024-03-20 19:01:14 +0100

    NBA cambios nuevos

--- src/practica1/equipobasket/entidades/NBALeague.java
+++ src/practica1/equipobasket/entidades/NBALeague.java
@@ -72,18 +72,58 @@ public class NBALeague {
      * @return el jugador más alto.
      */
     public JugadorBasket buscarJugadorAlto(){
+        //Evitar hacer esto
         final JugadorBasket[] jugadorAlto = {null};
         final Double[] alturaMaxima = {0.0};
 
         equipos.forEach(( nombre , equipo) -> {
-            equipo.getJugadores().forEach(jugadorBasket -> {
-                if (jugadorBasket.getAltura() > alturaMaxima[0]){
-                    alturaMaxima[0] = jugadorBasket.getAltura();
-                    jugadorAlto[0] = jugadorBasket;
+            equipo.getJugadores().forEach(jugador -> {
+                if (jugador.getAltura() > alturaMaxima[0]){
+                    alturaMaxima[0] = jugador.getAltura();
+                    jugadorAlto[0] = jugador;
                 }
             });
         });
         return jugadorAlto[0];
+        /*  Versión 1:
+            for(String equipo : equipos.keySet()){
+                for(JugadorBasket jugador : equipos.get(equipo).getJugadores()){
+                    if(jugador.getAltura() > alturaMaxima){
+                        alturaMaxima = jugador.getAltura();
+                        jugadorAlto = jugador;
+                    }
+                }
+            } */
+        /* Versión 2:
+            List<Jugador> jugadores = new ArrayList<>();
+            for (String equipo : equipos.keySet()){
+                jugadores.addAll(equipos.get(equipo).getJugadores());
+            }
+            Collections.sort(jugadores, new Comparator<jugador>(){
+            @Override
+            public int compare(Jugador o1, Jugador o2) {
+                return o1.getAltura().compareTo(o2);
+            }
+            } );
+            return jugadores.get(0);
+         */
+        /* Versión 3:
+        jugador j1 = null;
+        //Ordenan de menor a mayor, si queremos el mayor, comparamos al revés.
+            Set<Jugador> jugadores = new TreeSet<>((j1,j2) -> jugador2.queAltura().compareTo(jugador1.getAltura()));
+            for (String equipo : equipos.keySet()){
+                jugadores.addAll(equipos.get(equipo).getJugadores());
+            }
+            for(String jugador : jugadores){
+                if(jugador.getAltura() > alturaMaxima){
+                    jugador1 = jugador;
+                    break;
+                }
+                return jugador1;
+            }
+
+
+         */
     }
 
     /**
