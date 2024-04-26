e73eaf309fe9cf674816ae91183ebbe36c19bcd2
AdrianPerez2207 <aperezsanchez441@gmail.com>
2024-04-26 11:05:11 +0200

    examen 2023 avanzado

--- src/examen_2023/application/Test.java
+++ src/examen_2023/application/Test.java
@@ -9,12 +9,12 @@ import java.time.LocalDate;
 public class Test {
 
     public static void main(String[] args) {
-        RegistroDatosDia r1 = new RegistroDatosDia(1L, LocalDate.now(), 34.50, -10.5, 18.56);
-        RegistroDatosDia r2 = new RegistroDatosDia(2L, LocalDate.of(2024, 4, 23), 34.50, -10.5, 18.56);
-        RegistroDatosDia r3 = new RegistroDatosDia(3L, LocalDate.now(), 34.50, -10.5, 18.56);
+        RegistroDatosDia r1 = new RegistroDatosDia(1L, LocalDate.now(), 34.8, -10.5, 18.56);
+        RegistroDatosDia r2 = new RegistroDatosDia(2L, LocalDate.of(2024, 4, 23), 28.50, -10.5, 18.56);
+        RegistroDatosDia r3 = new RegistroDatosDia(3L, LocalDate.now(), 40.50, -10.5, 38.56);
 
         EstacionMeteorologica e1 = new EstacionMeteorologica(4L, "La serena", "Andalucia", 45.56, 23.76);
-        EstacionMeteorologica e2 = new EstacionMeteorologica(6L, "Tetica", "Andalucia", 50.56, 13.76);
+        EstacionMeteorologica e2 = new EstacionMeteorologica(6L, "Tetica", "Andalucía", 50.56, 13.76);
 
         e1.nuevoRegistro(r1);
         e1.nuevoRegistro(r2);
@@ -24,7 +24,25 @@ public class Test {
         s1.nuevaEstacion(e1);
         s1.nuevaEstacion(e2);
 
-
+        System.out.println("---------");
+        System.out.println(s1.getTempPorEstacion(4L));
+        System.out.println("---------");
         System.out.println(s1.getTempPorAnioEstacion(4L, 2024));
+        System.out.println(s1.getTempMaxRegistrada());
+
+        System.out.println("---------");
+        s1.getEstacionesPorComunidad().forEach((k,v) -> {
+            System.out.println(k);
+            v.forEach(System.out::println);
+        });
+        System.out.println("---------");
+        System.out.println(s1.isTempMediaAlta());
+        System.out.println("---------");
+        s1.pintaEstacionFarenheit(4L);
+        System.out.println("---------");
+        s1.getNumRegistros().forEach((k, v) -> {
+            System.out.println(k);
+            System.out.println(v);
+        });
     }
 }
--- src/examen_2023/service/ServicioGeneralMeteorologico.java
+++ src/examen_2023/service/ServicioGeneralMeteorologico.java
@@ -3,8 +3,7 @@ package examen_2023.service;
 import examen_2023.model.EstacionMeteorologica;
 import examen_2023.model.RegistroDatosDia;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ServicioGeneralMeteorologico {
@@ -45,6 +44,20 @@ public class ServicioGeneralMeteorologico {
     }
 
     /**
+     * Filtramos las estaciones, nos quedamos con los registros que coincidan con el Id, ordenamos por fecha y
+     * pasamos a lista.
+     * @param id
+     * @return
+     */
+    public List<RegistroDatosDia> getTempPorEstacion(Long id){
+        return estaciones.stream()
+               .filter(estacion -> estacion.getId().equals(id))
+               .flatMap(estacion -> estacion.getRegistros().stream())
+               .sorted(Comparator.comparing(RegistroDatosDia::getFecha))
+               .toList();
+    }
+
+    /**
      * Filtramos las estaciones que coinciden con el id.
      * Con FlatMap nos quedamos con un stream de los registros y filtramos por año.
      * @param id
@@ -55,7 +68,98 @@ public class ServicioGeneralMeteorologico {
         return estaciones.stream()
                .filter(estacion -> estacion.getId().equals(id))
                 .flatMap(estacion -> estacion.getRegistros().stream())
+                .sorted(Comparator.comparing(RegistroDatosDia::getTempMedia))
                 .filter(registro -> registro.getFecha().getYear() == anio)
                 .toList();
     }
+
+    /**
+     * Filtramos por comunidad, nos quedamos con los registros de esa comunidad, ordenamos por fecha y lo pasamos
+     * a lista.
+     * @param comunidad
+     * @return los registros pasados a alista.
+     */
+    public List<RegistroDatosDia> getTempPorComunidad(String comunidad){
+        return estaciones.stream()
+               .filter(estacion -> estacion.getComunidad().equals(comunidad))
+               .flatMap(estacion -> estacion.getRegistros().stream())
+               .sorted(Comparator.comparing(RegistroDatosDia::getFecha))
+               .toList();
+    }
+
+    /**
+     * Entramos en los registros de las estaciones, comparamos las temperaturas y nos quedamos con la mayor, en caso
+     * de no encontrar ninguna, saltará una excepción.
+     * @return la temperatura máxima registrada.
+     * @throws NoSuchElementException
+     */
+    public RegistroDatosDia getTempMaxRegistrada()throws NoSuchElementException {
+        return estaciones.stream()
+               .flatMap(estacion -> estacion.getRegistros().stream())
+               .max(Comparator.comparing(RegistroDatosDia::getTempMax))
+               .orElseThrow();
+    }
+
+    /**
+     * Agrupamos las estaciones meteorológicas por comunidades.
+     * @return Un mapa con las estaciones agrupadas.
+     */
+    public Map<String, List<EstacionMeteorologica>> getEstacionesPorComunidad(){
+        return estaciones.stream()
+               .collect(Collectors.groupingBy(EstacionMeteorologica::getComunidad));
+    }
+
+    /**
+     *Entramos en los registros de las estaciones, con anyMatch filtramos y comprobamos si hay algún registro
+     * mayor a 30º de temperatura media.
+     * @return true si encuentra un registro con la temperatura media mayor a 30º, si no encuentra, devuelve false.
+     */
+    public boolean isTempMediaAlta(){
+        return estaciones.stream()
+               .flatMap(estacion -> estacion.getRegistros().stream())
+               .anyMatch(registro -> registro.getTempMedia() > 30);
+    }
+    //devuelve un mapa agrupando por comunidad
+    //que contenga la comunidad y la lectura con la temperatura máxima en esa comunidad.
+
+    //public Map<String, Double> getTempMaxPorComunidad(){}
+    //.collect(Collectors.toMap(EstacionMeteorologica::getComunidad, estacion -> estacion.getRegistros().stream()
+    //                        .mapToDouble(RegistroDatosDia::getTempMax)
+    //                        .max()
+    //                        .orElseThrow()));
+
+    /**
+     * Filtramos las estaciones por comunidades, borramos todas las estaciones que estén dentro de esa comunidad.
+     * @param comunidad
+     */
+    public void borrarEstacionesComunidad(String comunidad){
+        estaciones.stream()
+                .filter(estacion -> estacion.getComunidad().equals(comunidad))
+                .forEach(estacion -> estaciones.remove(estacion));
+    }
+
+    /**
+     * Llamamos al método getTempPorEstacion, pintamos sus temperaturas pasadas a Farenheit.
+     * @param id
+     */
+    public void pintaEstacionFarenheit(Long id){
+        getTempPorEstacion(id)
+                .forEach(registro -> {
+                    System.out.println("Registro: " + registro.getId());
+                    System.out.println("Temperatura máxima: " + (registro.getTempMax() * 9 / 5) + 32);
+                    System.out.println("Temperatura mínima: " + (registro.getTempMin() * 9 / 5) + 32);
+                    System.out.println("Temperatura media: " + (registro.getTempMedia() * 9 / 5) + 32);
+                });
+    }
+
+    /**
+     * Agrupamos por estación, contamos los registros que tiene cada estación.
+     * @return
+     */
+    public Map<String, Long> getNumRegistros(){
+        return estaciones.stream()
+               .collect(Collectors.toMap(EstacionMeteorologica::getNombre, estacion -> estacion.getRegistros().stream()
+                       .count()));
+    }
+
 }
